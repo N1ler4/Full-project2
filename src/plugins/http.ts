6876d2dc27d07aaec7aeb0527274970293dc3142
N1ler4 <bahtiyorov.nozim@gmail.com>
2024-05-23 09:40:50 +0500

    Test

--- src/plugins/http.ts
+++ src/plugins/http.ts
@@ -1,26 +1,53 @@
 import axios from "axios";
-import { getDataFromCookie } from "../utils/tokenService.ts";
+import {
+  getDataFromCookie,
+  saveDataFromCookie,
+} from "../utils/tokenService.ts";
 
 const http = axios.create({
-    baseURL: "https://app.olimjanov.uz/v1"
+  baseURL: "https://app.olimjanov.uz/v1",
 });
 
-http.interceptors.request.use((config):any=>{
-    let token = getDataFromCookie("token");
-    if(token){
-        config.headers.Authorization = `${token}`;
-    }
-    return config
-})
+http.interceptors.request.use((config): any => {
+  let token = getDataFromCookie("token");
+  if (token) {
+    config.headers.Authorization = `${token}`;
+  }
+  return config;
+});
 
-async function refreshAccessToken(){
-    try{
-        const refresh_token = getDataFromCookie("refresh_token");
-        if(!refresh_token){
-            throw new Error("Refresh token not found")
-        }
-        const response = await http.post("/auth/refresh-accesstoken/", {refresh_token})
+async function refreshAccessToken() {
+  try {
+    const refresh_token = getDataFromCookie("refresh_token");
+    if (!refresh_token) {
+      throw new Error("Refresh token not found");
     }
+    const response = await axios.post(
+      `https://app.olimjanov.uz/v1/auth/refresh-accesstoken/${refresh_token}`
+    );
+    const { access_token } = response.data;
+    saveDataFromCookie("token", access_token);
+    return access_token;
+  } catch (err) {
+    console.log(err);
+  }
 }
+http.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      const access_token = await refreshAccessToken();
+      if (access_token) {
+        const originalRequest = error.config;
+        originalRequest.headers["Authorization"] = access_token;
+      } else {
+        console.error("Failed to refresh access token");
+        return Promise.reject(error);
+      }
+    }
+  }
+);
 
-export default http
+export default http;
